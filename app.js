d46ccd796dfeee8e09bc3d11a1a09e2e0b73f9a1
zedxog <zubydev2012@gmail.com>
2022-02-11 02:29:05 +0100

    fixed a bug

--- app.js
+++ app.js
@@ -58,11 +58,18 @@ app.use(function (req, res, next) {
         res.setHeader("Access-Control-Allow-Origin", origin); // restrict it to the required domain
     }
 
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader("Access-Control-Allow-Origin", "*")
       res.setHeader(
         "Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept, Authorization"
       );
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
     next();
 });
 app.use((req, res, next) => {
